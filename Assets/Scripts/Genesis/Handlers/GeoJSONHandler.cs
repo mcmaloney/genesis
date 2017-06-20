b7c32ff5964802eee1464d7f72c53f81544c6a2b
mcmaloney <maloney.mc@gmail.com>
2017-06-20 14:03:00 -0400

    Update GeoJSON handler to accept a feature collection with points and polygons

--- Assets/Scripts/Genesis/Handlers/GeoJSONHandler.cs
+++ Assets/Scripts/Genesis/Handlers/GeoJSONHandler.cs
@@ -25,7 +25,10 @@ public class GeoJSONHandler : MonoBehaviour {
         if (feature.geometry is GeoJSON.PointGeometryObject)
         {
             generatePointObject(feature);
-        } 
+        } else if (feature.geometry is GeoJSON.PolygonGeometryObject || feature.geometry is GeoJSON.MultiPolygonGeometryObject)
+        {
+            generatePolygonObject(feature);
+        }
     }
 
     public void generatePointObject(GeoJSON.FeatureObject feature)
@@ -33,5 +36,19 @@ public class GeoJSONHandler : MonoBehaviour {
         featureGenerator = featureGeneratorObject.GetComponent<FeatureGenerator>();
         Vector2d pointCoordiates = new Vector2d(feature.geometry.AllPositions()[0].latitude, feature.geometry.AllPositions()[0].longitude);
         featureGenerator.drawGeoPoint(pointCoordiates);
+    }
+
+    public void generatePolygonObject(GeoJSON.FeatureObject feature)
+    {
+        Vector2d[] latLonVertices = new Vector2d[feature.geometry.AllPositions().Count];
+
+        for (int i = 0; i < feature.geometry.AllPositions().Count; i++)
+        {
+            latLonVertices[i] = new Vector2d(feature.geometry.AllPositions()[i].latitude, feature.geometry.AllPositions()[i].longitude);
+            Debug.Log(latLonVertices[i]);
+        }
+
+        featureGenerator = featureGeneratorObject.GetComponent<FeatureGenerator>();
+        featureGenerator.drawGeoPolygon(latLonVertices);
     }
 }
