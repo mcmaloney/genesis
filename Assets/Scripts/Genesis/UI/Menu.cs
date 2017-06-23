656d213f8f4e40d6cb2c546ac22821f80732933d
mcmaloney <maloney.mc@gmail.com>
2017-06-23 18:37:27 -0400

    Fix scaling issues on instantiating ListItem

--- Assets/Scripts/Genesis/UI/Menu.cs
+++ Assets/Scripts/Genesis/UI/Menu.cs
@@ -13,10 +13,11 @@ namespace Genesis.UI
 
         public void addListItem(string labelValue)
         {
-            GameObject listItemObject = Instantiate(listItemPrefab, new Vector3(0f, listItemVerticalMargin - listItemStackHeight, 0f), Quaternion.Euler(-90f, 0f, 0f));
-            listItemObject.transform.localScale = new Vector3(0.25f, 1f, 0.06f);
-            listItemStackHeight += 0.75f;
+            GameObject listItemObject = Instantiate(listItemPrefab, new Vector3(0f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f));
             listItemObject.transform.parent = transform;
+            listItemObject.transform.localScale = new Vector3(0.25f, 1f, 0.06f);
+            listItemObject.transform.localPosition = new Vector3(0, listItemVerticalMargin - listItemStackHeight, 0f);
+            listItemStackHeight += 0.75f;
             listItemObject.GetComponent<ListItem>().labelTextValue = labelValue;
         }
     }
