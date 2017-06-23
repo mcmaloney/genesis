60769fa75a7d1801a6ae468d2dd6e799c9cfb1fa
mcmaloney <maloney.mc@gmail.com>
2017-06-23 13:53:41 -0400

    Add a world container object
    This is important, and points the way at future functionality. Before we only had Genesis create a single WorldRoot layer and populate it with tiles. If you changed location on the map, it'd destroy the world. In doing this, we now create the ability for the GenesisController to build multiple worlds with their own data layers simultaneously...

---
+++ Assets/Scripts/Genesis/Core/World.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Utils;
+
+namespace Genesis.Core
+{
+    public class World : MonoBehaviour
+    {
+        public string worldName;
+        public Vector2d originCoordinates;
+        public float worldScale;
+    }
+}
+
