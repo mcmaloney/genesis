444b0fbcf61bf8ca0a74221b59eb74e9922d3a1b
mcmaloney <maloney.mc@gmail.com>
2017-06-29 13:05:24 -0400

    All actions now associated with Hand

--- Assets/Scripts/Genesis/User/Player.cs
+++ Assets/Scripts/Genesis/User/Player.cs
@@ -7,37 +7,9 @@ namespace Genesis.User
 {
     public class Player : MonoBehaviour
     {
-        public GameObject genesisUIObject;
         public Vector3 UIOffset;
         public Hand rightHand; // Movement
         public Hand leftHand; // Tools
-        public float movementSpeed;
-
-        private UIController genesisUIController;
-
-        public void Start()
-        {
-            genesisUIController = genesisUIObject.GetComponent<UIController>();
-        }
-
-        public void Update()
-        {
-            if (OVRInput.GetDown(OVRInput.RawButton.Y))
-            {
-                toggleUI();
-            }
-        }
-
-        private void toggleUI()
-        {
-            if (!genesisUIObject.activeSelf)
-            {
-                genesisUIController.SetInitialState(transform.position + UIOffset);
-            } else
-            {
-                genesisUIController.Close();
-            }
-        }
     }
 }
 
