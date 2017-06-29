9e57ff92a0e8488a2c704bfeb62e698139e8cbcc
mcmaloney <maloney.mc@gmail.com>
2017-06-29 11:30:23 -0400

    Refactor the world

--- Assets/Scripts/Genesis/Core/World.cs
+++ Assets/Scripts/Genesis/Core/World.cs
@@ -13,54 +13,66 @@ namespace Genesis.Core
     public class World : MonoBehaviour
     {
         public MapVisualization MapVisualization;
-        public GameObject FeatureRenderer;
+        public GameObject FeatureRenderer; 
         public RectD ReferenceTileRect { get; set; }
         public float WorldScaleFactor { get; set; }
         public Vector2d RootTileOrigin { get; set; }
         public int Zoom;
         public Vector4 Range;
         public float TileSize = 100;
+        public Vector2d originReferencePoint = new Vector2d(0, 0);
         public Dictionary<Vector2, UnityTile> _tiles;
-        public float userZoomInput;
+        public int grabMaskLayerNumber = 9;
 
         public TextAsset testDataSource; // Need to replace with data from API calls!
 
         private List<DataSource> dataSources;
         private FeatureRenderer _FeatureRenderer;
+        private Vector2 rawTileSize;
+        private Vector2 tileGridSize;
         private float lastScale;
         private Vector3 scaleIncrement = new Vector3(0.001f, 0.001f, 0.001f);
 
-        public void Update()
-        {
-            Scale();
-        }
-
         public void Build(Vector2d coordinates, int zoom, Vector4 frame, string name)
         {
-            gameObject.name = name;
+            BuildWorldRootObject(name, grabMaskLayerNumber); // Grab mask. Should make this more configurable
+            BuildWorldPrimitives(OriginTileMeters(OriginDegreesToWorldMeters(coordinates, originReferencePoint), zoom), frame, zoom);
+            SetWorldScale(ReferenceTileRect.Size.x);
+            BuildTiles(OriginTileMeters(OriginDegreesToWorldMeters(coordinates, originReferencePoint), zoom), coordinates, frame, zoom);
+            AddPhysics();
+            BuildCollider();
+        }
 
+        private void BuildWorldRootObject(string name, int grabMaskLayerNumber)
+        {
+            gameObject.name = name;
+            gameObject.layer = grabMaskLayerNumber;
+        }
+
+        private Vector2d OriginDegreesToWorldMeters(Vector2d originCoordinatesDegrees, Vector2d referencePoint)
+        {
+            return Conversions.GeoToWorldPosition(originCoordinatesDegrees.y, originCoordinatesDegrees.x, referencePoint);
+        }
+
+        private Vector2 OriginTileMeters(Vector2d originTileMeters, int zoom)
+        {
+            return Conversions.MetersToTile(originTileMeters, zoom);
+        }
+
+        private void BuildWorldPrimitives(Vector2 tileMeters, Vector4 range, int zoom)
+        {
             MapVisualization.Initialize(MapboxAccess.Instance);
             _tiles = new Dictionary<Vector2, UnityTile>();
+            ReferenceTileRect = BuildReferenceTile(tileMeters, zoom);
+            RootTileOrigin = BuildRootTile((int)tileMeters.x, (int)tileMeters.y, zoom);
+            Range = range;
+            Zoom = zoom;
+        }
 
-            var v2 = Conversions.GeoToWorldPosition(coordinates.y, coordinates.x, new Vector2d(0, 0));
-            var tms = Conversions.MetersToTile(v2, zoom);
-
-            ReferenceTileRect = BuildReferenceTile(tms, zoom);
-            RootTileOrigin = BuildRootTile((int)tms.x, (int)tms.y, zoom);
-
-            WorldScaleFactor = (float)(TileSize / ReferenceTileRect.Size.x);
-
+        private void SetWorldScale(double refTileSize)
+        {
+            WorldScaleFactor = (float)(TileSize / refTileSize);
             transform.localScale = Vector3.one * WorldScaleFactor;
-
-            for (int i = (int)(tms.x - frame.x); i <= (tms.x + frame.z); i++)
-            {
-                for (int j = (int)(tms.y - frame.y); j <= (tms.y + frame.w); j++)
-                {
-                    var tile = BuildTile(coordinates, i, j, zoom);
-                    _tiles.Add(new Vector2(i, j), tile);
-                    MapVisualization.ShowTile(tile);
-                }
-            }
         }
 
         private RectD BuildReferenceTile(Vector2 rootTileMeters, int zoom)
@@ -75,6 +87,19 @@ namespace Genesis.Core
             return Conversions.GeoToWorldPosition(rootTileOriginCoordinates, new Vector2d(0, 0));
         }
 
+        private void BuildTiles(Vector2 originTileMeters, Vector2d originCoordinatesDegrees, Vector4 range, int zoom)
+        {
+            for (int i = (int)(originTileMeters.x - range.x); i <= (originTileMeters.x + range.z); i++)
+            {
+                for (int j = (int)(originTileMeters.y - range.y); j <= (originTileMeters.y + range.w); j++)
+                {
+                    var tile = BuildTile(originCoordinatesDegrees, i, j, zoom);
+                    _tiles.Add(new Vector2(i, j), tile);
+                    MapVisualization.ShowTile(tile);
+                }
+            }
+        }
+
         private UnityTile BuildTile(Vector2d coordinates, int xCoord, int yCoord, int zoom)
         {
             var tile = new GameObject("Tile - " + xCoord + " | " + yCoord).AddComponent<UnityTile>();
@@ -82,9 +107,28 @@ namespace Genesis.Core
             tile.RelativeScale = Conversions.GetTileScaleInMeters(0, zoom) / Conversions.GetTileScaleInMeters((float)coordinates.y, zoom);
             tile.TileCoordinate = new Vector2(xCoord, yCoord);
             tile.Rect = Conversions.TileBounds(tile.TileCoordinate, zoom);
+            rawTileSize = new Vector2((float)tile.Rect.Size.x, (float)tile.Rect.Size.y);
             tile.transform.position = new Vector3((float)(tile.Rect.Center.x - ReferenceTileRect.Center.x), 0, (float)(tile.Rect.Center.y - ReferenceTileRect.Center.y));
             tile.transform.SetParent(transform, false);
             return tile;
+        }
+
+        private void AddPhysics()
+        {
+            Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.useGravity = false;
+        }
+
+        private void BuildCollider()
+        {
+            tileGridSize = new Vector2((Range.x * 2) + 1, (Range.z * 2) + 1);
+            float xSize = tileGridSize.x * rawTileSize.x;
+            float colliderHeight = 443f; // should be tallest building height
+            float zSize = tileGridSize.y * rawTileSize.y;
+            BoxCollider bc = gameObject.AddComponent<BoxCollider>();
+            bc.center = new Vector3(0f, colliderHeight / 2, 0f);
+            bc.size = new Vector3(xSize, colliderHeight, zSize);
         }
 
         public void AddDataSource(TextAsset source)
@@ -105,24 +149,6 @@ namespace Genesis.Core
             dataSources = new List<DataSource>();
             AddDataSource(testDataSource);
             RenderDataSource(dataSources[0]);
-        }
-
-        public void Scale()
-        {
-            float currentScale = userZoomInput;
-            if (currentScale > 0 && transform.localScale.x > 0)
-            {
-                if (currentScale > lastScale)
-                {
-                    transform.localScale += scaleIncrement;
-                }
-                else
-                {
-                    transform.localScale -= scaleIncrement;
-                }
-            }
-
-            lastScale = currentScale;
         }
     }
 }
